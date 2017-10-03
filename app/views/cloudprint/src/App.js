00955b6b40b14969a9578b01ac422deaa40279b9
Talha Koc <talha.koc@duke.edu>
2017-10-03 00:18:57 +0000

    yo

--- app/views/cloudprint/src/App.js
+++ app/views/cloudprint/src/App.js
@@ -30,7 +30,5 @@ class App extends Component {
         );
     }
 }
-const style = {
-    margin: 15,
-};
+
 export default App;
--- app/views/cloudprint/src/Login.js
+++ app/views/cloudprint/src/Login.js
@@ -42,7 +42,6 @@ class Login extends Component {
     }
     handleClick(event) {
         var apiBaseUrl = "http://localhost:4000/api/";
-        var self = this;
         var payload = {
             "email": this.state.username,
             "password": this.state.password
@@ -50,13 +49,13 @@ class Login extends Component {
         axios.post(apiBaseUrl + 'login', payload)
             .then(function(response) {
                 console.log(response);
-                if (response.data.code == 200) {
+                if (response.data.code === 200) {
                     console.log("Login successfull");
                     // var uploadScreen = [];
                     // uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
                     // self.props.appContext.setState({ loginPage: [], uploadScreen: uploadScreen })
                 }
-                else if (response.data.code == 204) {
+                else if (response.data.code === 204) {
                     console.log("Username password do not match");
                     alert("username password do not match")
                 }
