ff8ddbfcca838e149f8bb35ca46fe2348b04cdaf
Mitchell <mrb73@duke.edu>
2017-10-25 05:35:07 +0000

    Styled navbar

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :abouts
   resources :logins
   resources :property_views
+  resources :contact_page
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
