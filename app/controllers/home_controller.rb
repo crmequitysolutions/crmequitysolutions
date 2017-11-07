a52f270bd0e9e46ebefa399d8de2dffb5c77f02d
Noah Over <nko2@duke.edu>
2017-11-07 02:23:59 +0000

    created home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @q = Contact.where(["user_email = ?", current_user.email]).ransack(params[:q])
+        @contacts = @q.result(distinct: true)
+        @q2 = Property.where(["user_email = ?", current_user.email]).ransack(params[:q])
+        @properties = @q2.result(distinct: true)
+    end
+    
+end
