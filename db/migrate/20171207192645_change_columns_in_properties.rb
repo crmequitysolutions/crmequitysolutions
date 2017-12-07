1093961edb86f6b52d4f87f0819856081ad310a2
Noah Over <nko2@duke.edu>
2017-12-07 20:22:21 +0000

    still trying

--- db/migrate/20171207192645_change_columns_in_properties.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnsInProperties < ActiveRecord::Migration[5.1]
-  def change
-    change_column :properties, :time_on_mrkt, 'float USING CAST(time_on_mrkt AS float)'
-    change_column :properties, :roof_age, 'float USING CAST(roof_age AS float)'
-    change_column :properties, :plumb_age, 'float USING CAST(plumb_age AS float)'
-    change_column :properties, :heat_age, 'float USING CAST(heat_age AS float)'
-    change_column :properties, :cool_age, 'float USING CAST(cool_age AS float)'
-  end
-end
